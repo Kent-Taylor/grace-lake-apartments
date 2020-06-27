b756b20a0374d6b569412dcefc0b7b86277bf635
107124 <kredfox4@gmail.com>
2020-06-27 13:37:05 -0700

    added a map page and a floor-plans page

---
+++ src/components/FloorPlans.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class FloorPlans extends Component {
+  render() {
+    return (
+      <div className='floor-plans-container'>
+        Floor Plans Page
+      </div>
+    );
+  }
+}
