e8cd93c426767ef6326cd5ce08974b3c3eecfc4b
107124 <kredfox4@gmail.com>
2020-07-01 16:44:18 -0600

    added photos to photo page

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -10,8 +10,27 @@ export default class Photos extends Component {
                 <Navigation />
 
                 <div className='photos-container'>
-                    photos page
-            </div>
+                        
+                        <img className="kitchen" src="https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2253&q=80" />
+
+                        <img className="bedroom" src="https://images.unsplash.com/photo-1540518614846-7eded433c457?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2239&q=80" />
+
+                        <img className="livingRoom" src="https://images.unsplash.com/photo-1580792897564-25cdec1395ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
+
+                        <img className="bathroom" src="https://images.unsplash.com/photo-1580793135067-3ae54b77f68b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
+
+                        <img className="pool" src="https://images.unsplash.com/photo-1572331165267-854da2b10ccc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" />
+
+                        <img className="gym" src="https://images.unsplash.com/photo-1571902943202-507ec2618e8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2168&q=80" />
+
+                        <img className="playground" src="https://images.unsplash.com/photo-1579231847384-9de04c6c0e36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
+
+                        <img className="cafe" src="https://images.unsplash.com/photo-1559305616-3f99cd43e353?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80" />
+
+                        <img className="study" src="https://images.unsplash.com/photo-1514369118554-e20d93546b30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" />
+
+
+                </div>
                 <Footer />
 
             </div>
