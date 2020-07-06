2ce8241373d3f4963585f60bcc92ee191960833b
107124 <kredfox4@gmail.com>
2020-07-06 17:29:30 -0400

    completed the contact page and changed styles to the floor plans

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,25 +11,41 @@ export default class Contact extends Component {
         }
 
     }
-
+    handleSubmit = () => {
+        console.log("button successfully clicked")
+        this.setState({
+            sendButton: false
+        })
+        event.preventDefault();
+    }
     render() {
         return (
             <div>
                 <Navigation />
                 <div className='contact-page-wrapper'>
-                    {/* <form onS */}
-                    <div className="left-side">
-                        <p className="get-in-touch-text">Get in touch:</p>
-                        <p className="subheading">Let us know what we can do to help!</p>
-                        <input className="contact-form" placeholder="Name" />
-                        <input className="contact-form" placeholder="Your Email" type="email" />
-                        <textarea className="contact-form" placeholder="Your Message" />
+                    <form onSubmit={this.handleSubmit} >
+                        <div className="left-side">
+                            <p className="get-in-touch-text">Get in touch:</p>
+                            <p className="subheading">Let us know what we can do to help!</p>
+                            <input className="contact-form" placeholder="Name" />
+                            <input className="contact-form" placeholder="Your Email" type="email" />
+                            <textarea className="contact-form" placeholder="Your Message" />
 
-                        {/* <button type="submit" className="send-button">{this.state.sendButton === true ? "Send" : "Message Sent"}</button> */}
+                            <button type="submit" className="send-button">{this.state.sendButton === true ? "Send" : "Message Sent!"}</button>
 
-                    </div>
+                        </div>
+                    </form>
                     <div className="right-side">
-                        Contact us:
+                        <div className="dark-wrapper">
+                            <p className="heading">Connect with us:</p>
+                            {/* <i class="fas fa-phone"></i> */}
+
+                            <p className="sub-heading">Main office: (214) 748-3647</p>
+                            {/* <i class="fas fa-paper-plane"></i> */}
+                            <p>2876  Nash Street
+                            Burr Ridge
+                        TN</p>
+                        </div>
                     </div>
                 </div>
                 <Footer />
